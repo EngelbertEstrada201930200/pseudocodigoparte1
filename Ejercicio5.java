156203706efa1a2ba9012cc29106594901da9c8e
Engelbert Estrada <engelbertestrada201930200@cunoc.edu.gt>
2021-06-09 02:45:04 -0600

    Ejercicio 5 java

---
+++ Ejercicio5.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Ejercicio5 {
+
+	public static void main (String [] args){
+
+		int num1;
+	
+		Scanner entrada = new Scanner (System.in);
+
+
+		System.out.println (" Ingrese un numero ");
+		num1 = entrada.nextInt();
+
+		num1++;
+
+		System.out.println(" El numero siguiente es "+ num1);
+
+	}
+
+
+}
