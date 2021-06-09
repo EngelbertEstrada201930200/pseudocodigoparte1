4d947aea81d9040bb709f9dd47f07c0a338641f5
Engelbert Estrada <engelbertestrada201930200@cunoc.edu.gt>
2021-06-09 02:46:53 -0600

    Ejercicio 7 java

---
+++ Ejercicio7.java
@@ -0,0 +1,17 @@
+
+public class Ejercicio7 {
+
+	public static void main (String[] args){
+
+		int num;
+
+		num = (int)(Math. random()*50+10);
+		System.out.println (" El numero aleatorios es : " +num);
+		
+
+		double disminuir = num*0.85 ;
+
+		System.out.println (" El numero disninuido el 15% es "+ disminuir );
+
+	}
+}
