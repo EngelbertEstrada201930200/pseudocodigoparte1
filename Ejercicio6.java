f61a88a4401d0bdcd3f38ff7aaba6ba2a36b6907
Engelbert Estrada <engelbertestrada201930200@cunoc.edu.gt>
2021-06-09 02:46:14 -0600

    Ejercicio 6 java

---
+++ Ejercicio6.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Ejercicio6{
+
+	public static void main (String[] args){
+
+		
+		int numero = (int)(Math. random()*200+0);
+
+
+		System.out.println("\nEl numero aleatorio es: " +numero);
+		double aumento = (numero*0.130);		
+
+		System.out.println("\nEl Aumeto de "+numero+" a un 30% es "+aumento);
+
+	}
+}
